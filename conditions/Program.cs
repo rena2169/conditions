76adca05eb5a202f7ca89f5ab61487485e7b3461
rena2169 <sufiyanov.renal@gmail.com>
2025-03-07 04:02:08 +0300

    conditions

---
+++ conditions/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Тема: Условия
+
+            // Сравнение двух чисел
+
+            //Console.Write("Введите число a: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите число b: ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //if (a > b)
+            //{
+            //    Console.WriteLine($"Число a {a} больше b {b}");
+            //}
+            //else if (a == b)
+            //{
+            //    Console.WriteLine("Числа равны!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Число b {b} больше a {a}");
+            //}
+
+            // Калькулятор скидки
+
+            //Console.Write("Сумма покупки: ");
+            //float totalPurchase = Convert.ToSingle(Console.ReadLine());
+            //float discountAmount;
+            //float totalPrice;
+
+            //if (totalPurchase <= 1000)
+            //{
+            //    Console.WriteLine($"Итоговая сумма к оплате: {totalPurchase}");
+            //}
+            //else if (totalPurchase > 1000)
+            //{
+            //    discountAmount = (totalPurchase * 10) / 100;
+            //    totalPrice = totalPurchase - discountAmount;
+
+            //    Console.WriteLine($"Итоговая сумма к оплате: {totalPrice}");
+            //}
+            //else
+            //{
+            //    discountAmount = (totalPurchase * 20) / 100;
+            //    totalPrice = totalPurchase - discountAmount;
+
+            //    Console.WriteLine($"Итоговая сумма к оплате: {totalPrice}");
+            //}
+
+            //Определение категории билета
+
+            //Console.Write("Введите свой возраст: ");
+            //int userAge = Convert.ToInt32(Console.ReadLine());
+
+            //if (userAge <= 5)
+            //{
+            //    Console.WriteLine("Детский билет");
+            //}
+            //else if (userAge <= 17)
+            //{
+            //    Console.WriteLine("Подростковый билет");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Взрослый билет");
+            //}
+
+            // Классификация температуры
+
+            //Console.Write("Введите температуру вохду в данный момент: ");
+            //int airTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (airTemperature < 0)
+            //{
+            //    Console.WriteLine("Холодно");
+            //}
+            //else if (airTemperature <= 15)
+            //{
+            //    Console.WriteLine("Прохладно");
+            //}
+            //else if (airTemperature <= 25)
+            //{
+            //    Console.WriteLine("Тепло");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Жарко");
+            //}
+
+            // Определение знака числа
+
+            //Console.Write("Введите число: ");
+            //int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+            //if (inputNumber > 0)
+            //{
+            //    Console.WriteLine("Число положительное");
+            //}
+            //else if (inputNumber < 0)
+            //{
+            //    Console.WriteLine("Число отрицательное");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Число равно нулю");
+            //}
+
+            // Определение времени года
+
+            //Console.Write("Введите номер месяца: ");
+            //String monthNumber = Console.ReadLine();
+
+            //switch (monthNumber)
+            //{
+            //    case "1":
+            //    case "2":
+            //    case "12":
+            //        Console.WriteLine("Зима");
+            //        break;
+            //    case "3":
+            //    case "4":
+            //    case "5":
+            //        Console.WriteLine("Весна");
+            //        break;
+            //    case "6":
+            //    case "7":
+            //    case "8":
+            //        Console.WriteLine("Лето");
+            //        break;
+            //    case "9":
+            //    case "10":
+            //    case "11":
+            //        Console.WriteLine("Осень");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Некорректный номер месяца.");
+            //        break;
+            //}
+
+            // Определение выходного дня
+
+            //Console.Write("Введите номер дня недели (1 - понедельник, ..., 7 - воскресенье): ");
+            //char dayNumber = Convert.ToChar(Console.ReadLine());
+
+            //switch (dayNumber)
+            //{
+            //    case '1':
+            //    case '2':
+            //    case '3':
+            //    case '4':
+            //    case '5':
+            //        Console.WriteLine("Рабочий день");
+            //        break;
+            //    case '6':
+            //    case '7':
+            //        Console.WriteLine("Выходной");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Некорректный номер дня нелели.");
+            //        break;
+            //}
+
+            // Расчет штрафа за скорость
+
+            //Console.Write("Введите скорость автомабиля: ");
+            //int carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            //if (carSpeed <= 60)
+            //{
+            //    Console.WriteLine("Без штрафа");
+            //}
+            //else if (carSpeed <= 80)
+            //{
+            //    Console.WriteLine("Штраф 500 рублей");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Штраф 2000 рублей");
+            //}
+        }
+    }
+}
