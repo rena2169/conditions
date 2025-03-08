7aae6d9d8c77ffe337cd5e0a5afebeaf8bff83bf
rena2169 <sufiyanov.renal@gmail.com>
2025-03-08 23:11:57 +0300

    Update Program.cs

--- conditions/Program.cs
+++ conditions/Program.cs
@@ -119,28 +119,28 @@ namespace conditions
             // Определение времени года
 
             //Console.Write("Введите номер месяца: ");
-            //String monthNumber = Console.ReadLine();
+            //int monthNumber = Convert.ToInt32(Console.ReadLine());
 
             //switch (monthNumber)
             //{
-            //    case "1":
-            //    case "2":
-            //    case "12":
+            //    case 1:
+            //    case 2:
+            //    case 12:
             //        Console.WriteLine("Зима");
             //        break;
-            //    case "3":
-            //    case "4":
-            //    case "5":
+            //    case 3:
+            //    case 4:
+            //    case 5s:
             //        Console.WriteLine("Весна");
             //        break;
-            //    case "6":
-            //    case "7":
-            //    case "8":
+            //    case 6:
+            //    case 7:
+            //    case 8:
             //        Console.WriteLine("Лето");
             //        break;
-            //    case "9":
-            //    case "10":
-            //    case "11":
+            //    case 9:
+            //    case 10:
+            //    case 11:
             //        Console.WriteLine("Осень");
             //        break;
             //    default:
